0ef86809228544c301392313d5ca8e1cf69f1951
unknown <nguyenlamvu123@gmail.com>
2021-12-29 23:23:28 +0700

    '2912'

--- pandas_bigdata/homwk.py
+++ pandas_bigdata/homwk.py
@@ -40,41 +40,47 @@ class Baitapbuoi4:
 
     def Ex3(self):
         df = pd.read_csv('out.csv')
-        df.loc[0] = df.loc[0].astype(str)+ "my_first_name"
-        df.loc[1] = df.loc[1].astype(str)+ "my_last_name"
+        df.loc[0] = df.loc[0].astype(str)+ '#'#"my_first_name"
+        df.loc[1] = df.loc[1].astype(str)+ '@'#"my_last_name"
         print('&&&&&&&&&&', df.to_string())
-        return df
+        return self.Ex4(df)
 
-    def Ex4(self):
-        df = self.Ex3()
+    def Ex4(
+        self,
+        df
+        ):
+##        df = self.Ex3()
         print(
+            'all rows whose "country" is "Vietnam"', 
             df.loc[
                 :,
                 df.loc[4]=="Vietnam"
-                ]
+                ],
             )
         print(
+            'all rows whose "country" is not "Vietnam"',
             df.loc[
                 :,
                 df.loc[4]!="Vietnam"
-                ]
+                ],
             )
         print(
+            'all rows whose "age" is lower than 20',
             df.loc[
                 :,
-                df.loc[2]<20
-                ]
+                df.loc[2].astype(int)<20
+                ],
             )
         
 ##    for s in danhsach:
 ##        print(s)            
 s1 = []
 for zs in ["first_name", "last_name", "age", "job", "country"]:
-    ss = [zs+ str(i) for i in range(1,11)]
+    ss = [zs+ str(i) for i in range(1,11) if zs != "age"]
     if zs == "country":
         ss[3] = "Vietnam"
     elif zs == "age":
-        ss[7] = 20
+        ss = [20+ ii for ii in range(-12, 38, 5)]
     s1.append(ss)#;print(ss)
 
 s = Baitapbuoi4()
@@ -84,7 +90,6 @@ for ss in s1:
         )
 ##for ss in s.danhsach:
 ##    print(ss)
-print(s.Ex2_createdf().to_string())
-s.Ex2_csv()
-s.Ex3()
-s.Ex4()
+####print(s.Ex2_createdf().to_string())
+####s.Ex2_csv()
+####s.Ex3()
